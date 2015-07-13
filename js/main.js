b24bf51c00a78bc352c6972ba156495cff305a21
kevind <kevind@altierre.com>
2015-07-13 15:43:07 -0700

    Added 4th and 5th.

--- js/main.js
+++ js/main.js
@@ -26,8 +26,10 @@ Cat.prototype.getName = function() {
 var cat1 = new Cat("Cat1","images/catpic.jpg");
 var cat2 = new Cat("Cat2","images/cat2.jpg");
 var cat3 = new Cat("Cat3","images/cat3.jpg");
+var cat4 = new Cat("Cat4","images/cat4.jpg");
+var cat5 = new Cat("Cat5","images/cat5.jpg");
 
-var cats = [cat1,cat2,cat3];
+var cats = [cat1,cat2,cat3,cat4,cat5];
 
 // put the first cat name in the first span tag
  $("span").html(cat1.name);
